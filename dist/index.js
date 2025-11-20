7eebee2a359c8e6d530396a50f1ce4d7f5827cc7
valselviz <valeriaselviz@gmail.com>
2025-11-20 14:00:08 -0300

    Remove dist from repo

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const app = express();
-app.get("/", (req, res) => {
-    res.send("hello world");
-});
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
